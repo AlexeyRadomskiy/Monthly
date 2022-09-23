9fda86e73eb313f41425acb0c3b8b69c0509875f
AlexeyRadomskiy <alexeyradomskiy98@gmail.com>
2022-09-23 11:24:22 +0300

    Initial Commit

---
+++ Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by Alexey Radomskiy on 20.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
